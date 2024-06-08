31cf22b8d2c033c162862ccf099e673f313eb825
bo <q463250938@gmail.com>
2024-06-08 14:18:24 -0700

    first commit

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -6,16 +6,60 @@
 //
 
 import SwiftUI
+import MapKit
+import LocalAuthentication
+import WebKit
+import Kingfisher
+
+struct YoutubeView: UIViewRepresentable {
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: UIViewType, context: Context) {
+        let request = URLRequest(url: URL(string: "https://youtube.com")!)
+        uiView.load(request)
+    }
+}
 
 struct ContentView: View {
+    @State private var isUnlocked: Bool = false
+    @State private var isShowingYoutube: Bool = false
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Button(action: {
+                isShowingYoutube.toggle()
+            }, label: {
+                HStack {
+                    Image(systemName: "play")
+                    Text("Youtube")
+                }
+            })
+            .sheet(isPresented: $isShowingYoutube, content: {
+                YoutubeView()
+            })
         }
-        .padding()
+    }
+    
+    func authenticate() {
+        let context: LAContext = LAContext()
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "We need to unlock your data."
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) { success, authError in
+                if success {
+                    //authenticated successfully
+                    isUnlocked = true
+                } else {
+                    //there was a problem
+                }
+            }
+        } else {
+            
+        }
     }
 }
 
---
+++ Podfile
@@ -0,0 +1,10 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'BucketList' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for BucketList
+  pod 'Kingfisher', '~> 7.0'
+end
